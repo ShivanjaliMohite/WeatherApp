0b07209d04690db876571d069407dbe3d342f40b
ShivanjaliMohite <125596003+ShivanjaliMohite@users.noreply.github.com>
2023-02-28 10:18:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+/*let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+const getWeatherData=(city)=>
+  {
+     const URL = "https://api.openweathermap.org/data/2.5/weather";
+     const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+    const weatherpromimse=fetch(FULL_URL)
+    return weatherpromise.then((response) =>
+      {
+        return response.json()
+      })
+  }
+
+const searchCity=()=>
+  {
+    const city=document.getElementById("city-input").value
+    getWeatherData(city).then((res)=>{
+      showData(res)
+    }).catch((error)=>
+    {
+     console.log(error);
+      console.log("Something happened");
+    })
+  }
+const showData =(data)=>
+  {
+    document.getElementById("city-name").innerText=data.name;
+    document.getElementById("weather-type").innerText=data.weather[0].main
+    document.getElementById("temp").innerText=data.weather.temp
+    document.getElementById("min-temp").innerText=data.weather.temp_min
+    document.getElementById("max-temp").innerText=data.weather.temp_max
+  }
+*/
+/**
+ * Weather App
+ */
+
+// API_KEY for maps api
+let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+
+/**
+ * Retrieve weather data from openweathermap
+ */
+const getWeatherData = (city) => {
+  const URL = "https://api.openweathermap.org/data/2.5/weather";
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+  const weatherPromise  = fetch(FULL_URL);
+  return weatherPromise.then((response) => {
+    return response.json();
+  })
+}
+
+/**
+ * Retrieve city input and get the weather data
+ */
+const searchCity = () => {
+  const city = document.getElementById('city-input').value;
+  getWeatherData(city)
+  .then((res)=>{
+    showWeatherData(res);
+  }).catch((error)=>{
+    console.log(error);
+    console.log("Something happend");
+  })
+}
+
+/**
+ * Show the weather data in HTML
+ */
+showWeatherData = (weatherData) => {
+  document.getElementById("city-name").innerText = weatherData.name;
+  document.getElementById("weather-type").innerText = weatherData.weather[0].main;
+  document.getElementById("temp").innerText = weatherData.main.temp;
+  document.getElementById("min-temp").innerText = weatherData.main.temp_min;
+  document.getElementById("max-temp").innerText = weatherData.main.temp_max;
+}
+
